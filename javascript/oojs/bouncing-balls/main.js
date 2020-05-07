e526e072aacb3ce12ebc377516b5266303db628d
Niko Felger <niko.felger@gmail.com>
2020-05-07 09:51:02 +0200

    Bring bouncing balls JS in line with tutorial text
    On https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_building_practice, the random function generates a number in [min, max], whereas in the provided main.js file it is [min, max).

--- javascript/oojs/bouncing-balls/main.js
+++ javascript/oojs/bouncing-balls/main.js
@@ -8,7 +8,7 @@ const height = canvas.height = window.innerHeight;
 
 // function to generate random number
 
-function random(min,max) {
-  const num = Math.floor(Math.random()*(max-min)) + min;
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
