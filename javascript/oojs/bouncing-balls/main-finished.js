b54769193ae0728b709dfde4c79a17d718715439
Alpha <alcfa.onway@gmail.com>
2021-11-19 20:57:21 -0600

    edge misspell
    edge, not adge on line 83

--- javascript/oojs/bouncing-balls/main-finished.js
+++ javascript/oojs/bouncing-balls/main-finished.js
@@ -80,7 +80,7 @@ while(balls.length < 25) {
   const size = random(10,20);
   let ball = new Ball(
     // ball position always drawn at least one ball width
-    // away from the adge of the canvas, to avoid drawing errors
+    // away from the edge of the canvas, to avoid drawing errors
     random(0 + size,width - size),
     random(0 + size,height - size),
     random(-7,7),
