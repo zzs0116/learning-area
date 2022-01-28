17867da65fdcbe4e254efbf17ca66df2cb99a24f
IblackdI <98597503+IblackdI@users.noreply.github.com>
2022-01-28 16:01:54 +0200

    Update main.js

--- javascript/oojs/bouncing-balls/main.js
+++ javascript/oojs/bouncing-balls/main.js
@@ -12,3 +12,9 @@ function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
